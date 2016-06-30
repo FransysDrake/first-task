50750e24edb7bd5ce9d13236bef33fdb402df255
FransysDrake <fransysdrake@mail.ru>
2016-07-01 00:25:10 +0600

    Without OOP

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,67 @@
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("HELLO");
+        int[][] matrixA;
+        int[][] matrixB;
+        int[][] matrixC;
+        matrixA = new int[2][3];
+        matrixB = new int[3][3];
+        matrixC = new int[3][3];
+
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 3; j++) {
+                matrixA[i][j] = ((int) (Math.random() * 20));
+            }
+
+        }
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                matrixB[i][j] = ((int) (Math.random() * 20));
+            }
+        }
+
+
+
+
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(matrixA[i][j] + "\t");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(matrixB[i][j] + "\t");
+            }
+            System.out.println();
+        }
+
+
+        int m = matrixA.length;
+        int n = matrixB[0].length;
+        int o = matrixB.length;
+       // int[][] matrixC = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < o; k++) {
+                    matrixC[i][j] += matrixA[i][k] * matrixB[k][j];
+                }
+            }
+        }
+        System.out.println();
+
+        for (int i = 0; i < matrixC.length; i++) {
+            for (int j = 0; j < matrixC[0].length; j++) {
+                System.out.print(matrixC[i][j] + "\t");
+            }
+            System.out.println();
+        }
+
     }
-}
+    }
+
