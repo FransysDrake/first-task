528aac039530affcf94e3c38743b063510c8b9a4
FransysDrake <fransysdrake@mail.ru>
2016-06-30 18:47:18 +0600

    Step0

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,4 +2,7 @@
  * Created by Artur on 30.06.2016.
  */
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("HELLO");
+    }
 }
--- src/main/java/MatrixMultiplier.java
+++ src/main/java/MatrixMultiplier.java
@@ -2,4 +2,7 @@
  * Created by Artur on 30.06.2016.
  */
 public class MatrixMultiplier {
+    public static void main(String[] args) {
+
+    }
 }
