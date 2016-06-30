bc6f76e3997adbe5aeaf5570aeb2444e63bd0e40
FransysDrake <fransysdrake@mail.ru>
2016-07-01 00:32:47 +0600

    MatrixGeneration and Multiplier

--- src/main/java/MatrixGeneration.java
+++ src/main/java/MatrixGeneration.java
@@ -1,5 +1,39 @@
 /**
  * Created by Artur on 30.06.2016.
  */
+
 public class MatrixGeneration {
+
+        int[][] matrixA;
+        int[][] matrixB;
+
+
+        public  void generation(){
+
+            matrixA = new int[2][3];
+            matrixB= new int[3][3];
+
+            for (int i = 0; i < 2; i++) {
+                for (int j = 0; j < 3; j++) {
+                    matrixA[i][j] = ((int) (Math.random() * 20));
+                }
+
+            }
+
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    matrixB[i][j] = ((int) (Math.random() * 20));
+                }
+            }
+
+        }
+
+    public int[][] getMatrixA(int i,int j){
+        return matrixA;
+    }
+
+
+    public int[][] getMatrixB(int i, int j){
+        return matrixB;
+    }
 }
--- src/main/java/MatrixMultiplier.java
+++ src/main/java/MatrixMultiplier.java
@@ -2,7 +2,21 @@
  * Created by Artur on 30.06.2016.
  */
 public class MatrixMultiplier {
-    public static void main(String[] args) {
+    int[][] matrixC;
+    int [][] mA = matrixA.getMatrixA();
+    int [][] mB = matrixB.getMatrixB();
+
+
+    public void Multiplier() {
+        matrixC=new int[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                for (int k = 0; k < 3; k++) {
+                    matrixC[i][j] += mA.[i][j]*mB[k][j];
+                }
+            }
+        }
 
     }
+
 }
